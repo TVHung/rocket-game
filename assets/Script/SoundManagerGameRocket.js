47128d638b3c0edf953f02347f94d2cb906d25d1
truong hung <truonghungvatvo@gmail.com>
2021-03-06 20:48:51 +0700

    fix bug

--- assets/Script/SoundManagerGameRocket.js
+++ assets/Script/SoundManagerGameRocket.js
@@ -11,71 +11,105 @@
 cc.Class({
     extends: cc.Component,
     editor: {
-        menu:"CustomComponent/AudioControl",
+        menu: "CustomComponent/AudioControl",
     },
     properties: {
         backGroupSound: {
             default: null,
-            type: cc.AudioClip
+            type: cc.AudioClip,
         },
 
         loop: true,
-        
+
         soundVolume: {
             default: 1,
-            range: [0,1,0.1],
+            range: [0, 1, 0.1],
             slide: true,
-            notify: function() {
+            notify: function () {
                 this.setVolume();
-            }
+            },
         },
 
         audioClipPool: {
             default: [],
-            type: cc.AudioClip
+            type: cc.AudioClip,
         },
-        
+
         _isPlaying: false,
         _audioId: null,
         _EffectId: null,
     },
 
     //Âm thanh nền
-    playBackGroundSound () {
+    playBackGroundSound() {
         cc.audioEngine.stopAll();
-        this._audioId = cc.audioEngine.play(this.backGroupSound, this.loop, this.soundVolume);
+        this._audioId = cc.audioEngine.play(
+            this.backGroupSound,
+            this.loop,
+            this.soundVolume
+        );
     },
 
     //hiệu ứng âm thanh khi chơi game
-    playEffectSound (command, loop) {
+    playEffectSound(command, loop) {
         if (loop === null && loop === undefined) {
             var loop = this.loop;
         }
         if (command !== null || command !== undefined) {
             switch (command) {
-                case "begin":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[0], loop);
+                case "onClick":
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[0],
+                        loop
+                    );
                     break;
-                case "second":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[1], loop);
+                case "trueClick":
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[8],
+                        loop
+                    );
+                    break;
+                case "wrongClick":
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[1],
+                        loop
+                    );
                     break;
                 case "lose":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[2], loop);
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[2],
+                        loop
+                    );
                     break;
                 case "win":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[3], loop);
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[3],
+                        loop
+                    );
                     break;
                 case "batdau":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[4], loop);
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[4],
+                        loop
+                    );
                     break;
                 case "tieptuc":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[5], loop);
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[5],
+                        loop
+                    );
                     break;
                 case "hoanthanh":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[6], loop);
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[6],
+                        loop
+                    );
                     break;
                 case "cancogang":
-                    this._EffectId = cc.audioEngine.playEffect(this.audioClipPool[7], loop);
+                    this._EffectId = cc.audioEngine.playEffect(
+                        this.audioClipPool[7],
+                        loop
+                    );
                     break;
                 default:
                     console.error("Command is invalid");
@@ -83,20 +117,20 @@ cc.Class({
         }
     },
 
-    pauseMusic () {
-        cc.audioEngine.pauseAll();      //dừng tất cả âm thanh
+    pauseMusic() {
+        cc.audioEngine.pauseAll(); //dừng tất cả âm thanh
     },
 
-    resumeMusic () {
-        cc.audioEngine.resumeAll();     //tạm dừng tất cả âm thanh
+    resumeMusic() {
+        cc.audioEngine.resumeAll(); //tạm dừng tất cả âm thanh
     },
 
     setVolume() {
-        cc.audioEngine.setVolume(this.soundVolume);     //thiết lập âm lượng
+        cc.audioEngine.setVolume(this.soundVolume); //thiết lập âm lượng
     },
 
-    stopAll () {
-        cc.audioEngine.stopAll();       //dừng lại tất cả
+    stopAll() {
+        cc.audioEngine.stopAll(); //dừng lại tất cả
         this._audioId = null;
     },
 });
